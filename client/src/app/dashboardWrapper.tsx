f708ff5306b8dfaf89680ffb946c0d428cf2febf
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-12-23 11:58:16 +0530

    create dashboard wrapper file

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
   return (
-    <div>
-      Dashboard Wrapper
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+        {/* sidebar */}
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+            {/* navbar */}
+            {children}
+        </main>
     </div>
   )
 }
