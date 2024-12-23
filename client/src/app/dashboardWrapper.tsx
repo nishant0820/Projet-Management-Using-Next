c485fd475c01c3952dee639b82135881e98a6e76
Nishant Gupta <gupta.nishant08042004@gmail.com>
2024-12-23 11:45:19 +0530

    install tailwindcss

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardWrapper = ({ children }: {children: React.ReactNode}) => {
+  return (
+    <div>
+      Dashboard Wrapper
+    </div>
+  )
+}
+
+export default DashboardWrapper
